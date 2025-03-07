2cf06a006ae8a45d36bba7e9e0e3f3ae6895aecc
Allan Barcelos <allan@barcelos.dev>
2025-03-07 15:20:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Task Management System
+
+## Api X APP
+
+### Api
+- Backend
+- business logic
+- Java, C#, Nodejs, Python ...
+
+### App
+- Frontend
+- interact with users 
+- Html, CSS, Javascript ...
+- Frameworks: Angular, VueJS, ReactsJS ...
+
+## Docker Compose
+
+- need run in root foldert where you have the docker-compose.yml
+- `docker-compose up` to run or `docker-compose up -d` to rnu in background
+- `docker-compose down` to destroy 
+
+## VSCode X Visual STudio
+dotnet new 
+### VSCode
+- API
+- Frontend
+- Console Applications
+
+### Visual Studio (more resources to execute)
+- Windows APP
+- Mobile App
+- API (not common)
+
+
+## Troubleshot 
+
+`packagesourcemapping is enable ... the following suorce were not considered ... `
+
+- remove the `Nuget` folder `~/AppData/Roamin/Nuget` (Windows)
---
+++ api/Program.cs
@@ -0,0 +1,60 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+// Home http://localhost:<PORT>/
+app.MapGet("/", () =>
+{
+    return "API is working";
+})
+.WithName("GetHome");
+
+// In class task 30 min
+// implement a new endpoint "/{prince}/{tax}/"
+// return a JSON
+
+// { 
+//      price: 0.00
+//      tax: 0%
+//      final: price + tax
+// }
+
+
+// Weather Forecast "http://localhost:<PORT>/weatherforecast"
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
