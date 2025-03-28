8ee5c49b028f271f72046deace657ececf2a60f7
ThomasFregoneze <thomfregon@gmail.com>
2025-03-28 14:37:46 -0400

    Filter in the GET /tasks endpoint to list tasks by due date implemented

---
+++ api/Controllers/TaskController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("/[controller]")] // /tasks
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly YourDbContext _context;
+
+        public TasksController(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /tasks?startDate=2023-01-01&endDate=2023-01-31
+        [HttpGet]
+        public async Task<IActionResult> GetTasks([FromQuery] DateTime? startDate, [FromQuery] DateTime? endDate)
+        {
+            var query = _context.Tasks.AsQueryable();
+
+            if (startDate.HasValue && endDate.HasValue)
+            {
+                query = query.Where(t => t.DueDate >= startDate.Value && t.DueDate <= endDate.Value);
+            }
+
+            var tasks = await query.ToListAsync();
+            return Ok(tasks);
+        }
+    }
+}
