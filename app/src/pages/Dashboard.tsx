c30847f02bbff36dc65b0057be27b4d2a5e7ba0c
ThomasFregoneze <thomfregon@gmail.com>
2025-03-28 15:47:50 -0400

    Filter tasks by date front end implemented

--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -1,29 +1,46 @@
-import { useAuth } from '@hooks/useAuth';
-import api from '@config/axiosConfig';
-import { useEffect, useState } from 'react';
-import { toastr } from '@utils/toastr';
-
-const Dashboard = () => {
-  const { user } = useAuth();
-
-  // Example to use Axios config  (check the axiosConfig.ts for more informations)
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    api.get('/')
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        toastr('error', `Erro: ${error.message}`);
-      });
-  }, []);
-
-  return (
-    <div>
-      <p>Welcome, {user?.sub ?? 'No Name'}</p>
-    </div>
-  );
-};
-
-export default Dashboard;
+  import { useAuth } from '@hooks/useAuth';
+  import api from '@config/axiosConfig';
+  import { useEffect, useState } from 'react';
+  import { toastr } from '@utils/toastr';
+  import DateFilter from '../components/DateFilter';
+  import { Task } from '../interfaces/Task';
+  
+  const Dashboard = () => {
+    const { user } = useAuth();
+    const [tasks, setTasks] = useState<Task[]>([]);
+   // Example to use Axios config  (check the axiosConfig.ts for more informations)
+    const handleDateFilterChange = (startDate: string, endDate: string) => {
+      api.get(`/tasks?startDate=${startDate}&endDate=${endDate}`)
+        .then(response => {
+          setTasks(response.data);
+        })
+        .catch(error => {
+          toastr('error', `Erro: ${error.message}`);
+        });
+    };
+  
+    useEffect(() => {
+      api.get('/tasks')
+        .then(response => {
+          setTasks(response.data);
+        })
+        .catch(error => {
+          toastr('error', `Erro: ${error.message}`);
+        });
+    }, []);
+  
+    return (
+      <div>
+        <p>Welcome, {user?.sub ?? 'No Name'}</p>
+        <DateFilter onFilterChange={handleDateFilterChange} />
+        {tasks.map(task => (
+          <div key={task.id}>
+            {task.title} - Due: {task.dueDate}
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Dashboard;
+  
