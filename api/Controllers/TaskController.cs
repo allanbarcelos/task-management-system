6e6f47f115776f188ee5562bc5aa52d73cffed59
Andres Bonilla <andres94b@gmail.com>
2025-04-07 01:25:01 -0400

    Added a TaskController,Task History DbSet and get endpoint for task history list

---
+++ api/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+namespace API.Controllers{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Authorization;
+    using API.Data;
+
+    //Create a TaskHistory model to record status changes and add a GET /tasks/{id}/history endpoint to view the history.
+
+    public class TaskHistory{
+        public int Id { get; set; }
+        public int TaskItemId { get; set; }
+        public string OldStatus { get; set; } = string.Empty;
+        public string NewStatus { get; set; } = string.Empty;
+        public DateTime ChangedAt { get; set; } = DateTime.UtcNow;
+        public string ChangedByUserId { get; set; } = string.Empty;
+    }
+
+
+    [ApiController]
+    [Route("/[controller]")]
+    public class TaskController : ControllerBase{
+
+        private readonly ApplicationDbContext _dBContext;
+
+        public TaskController(ApplicationDbContext dBContext){
+            _dBContext = dBContext;
+        }
+
+        [HttpGet("/{id}/history")]
+        [Authorize(Policy="RequireUserRole")]
+        public async Task<IActionResult> getTaskHistory(int id){
+            var taskHistory =  await _dBContext.TasksHistories.Where(t => t.TaskItemId == id).ToListAsync();
+            
+            return Ok(taskHistory);
+        }
+
+    }
+
+}
---
+++ api/Migrations/20250407045343_AddTaskHistory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TasksHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TaskItemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OldStatus = table.Column<string>(type: "TEXT", nullable: false),
+                    NewStatus = table.Column<string>(type: "TEXT", nullable: false),
+                    ChangedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ChangedByUserId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TasksHistories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TasksHistories");
+        }
+    }
+}
