d1a464340ed90c30f56e854cc8b07874b272f772
Thai Duy Roan <duy.roan@gmail.com>
2025-04-08 12:00:14 -0400

    feat: add task model by status (closes #42)

--- app/src/interfaces/User.ts
+++ app/src/interfaces/User.ts
@@ -1,12 +1,4 @@
-export interface Task {
-    id: number;
-    title: string;
-    description?: string;
-    dueDate: Date;
-    status: string;
-    userId: string;        
-    reviewerId: string;
-    createdAt: Date;
-    updatedAt: Date;
-    
-  }
+export interface User {
+    sub: string;
+    roles: string[];
+}
